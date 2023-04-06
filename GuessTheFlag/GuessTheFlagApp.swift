5ada0a4dbb9bb9048db68785c144b59cba9cb17c
silvaton <tdasilva@ext.laligatech.com>
2023-04-06 16:38:56 +0200

    Initial commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Ton Silva on 6/4/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

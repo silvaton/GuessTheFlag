067eeb0b7e550d2be2e223d00c83aab09c30c732
silvaton <tdasilva@ext.laligatech.com>
2023-04-08 16:50:46 +0200

    Created the quiz layout

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var countriesList = ["Argentina", "Brazil", "Estonia", "France", "Germany", "Ghana", "Ireland", "Israel", "Italy", "Monaco", "Morroco", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            VStack(spacing: 20) {
+                VStack {
+                    Text("Tap the flag of")
+                        .font(.title)
+                    Text(countriesList[correctAnswer])
+                        .font(.title)
+                        .bold()
+                }
+                .padding()
+                
+                ForEach(0..<3) { number in
+                    Button {
+                        //
+                    } label: {
+                        Image(countriesList[number].lowercased())
+                            .resizable()
+                            .frame(maxWidth: 240, maxHeight: 120)
+                    }
+                    .cornerRadius(10)
+                    .shadow(radius: 10)
+                    
+                }
+            }
         }
-        .padding()
     }
 }
 
